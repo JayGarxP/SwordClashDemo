bdf97fbf54993685c5caac90f45f0bb487f17170
JP Fallon <JayGarxP@users.noreply.github.com>
2019-03-11 23:00:34 -0400

    Fixed p2 bubble bug with particle gravity.

--- Assets/SWORDCLASH/Scripts/ParticleBubsTest.cs
+++ Assets/SWORDCLASH/Scripts/ParticleBubsTest.cs
@@ -7,13 +7,28 @@ public class ParticleBubsTest : MonoBehaviour {
     // set manually in editor; did not put prefab in resources folder, but it is in scene
     public ParticleSystem BubbleParticlePrefab;
 
+    public ParticleSystem BubblePSystemPlayer2;
+    bool AmIPlayer2;
+
     // Use this for initialization
     void Start () {
-		
+        AmIPlayer2 = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        // If player two object is intantiated, we are player2
+        var Player2DummyObject = GameObject.FindWithTag("Player2");
+        if (Player2DummyObject != null && Player2DummyObject.tag != "/")
+        {
+            AmIPlayer2 = true;
+        }
+
+        if (BoltNetwork.IsClient)
+        {
+            AmIPlayer2 = true;
+        }
 
         Input.simulateMouseWithTouches = true;
         foreach (Touch touch in Input.touches)
@@ -23,12 +38,26 @@ public class ParticleBubsTest : MonoBehaviour {
             // Emit is smart enough to NOT emit if they already exist!!!
             //BubbleParticlePrefab.Emit(1);
             //}
-            EmitBubble();
+            if (AmIPlayer2)
+            {
+                EmitP2Bubbles();
+            }
+            else
+            {
+                EmitBubble(); //looks good on low dpi/low touch count device; might look bad on high quality phone
+            }
         }
 
         if (Input.GetMouseButtonDown(0))
         {
-            EmitBubble();
+            if (AmIPlayer2)
+            {
+                EmitP2Bubbles();
+            }
+            else
+            {
+                EmitBubble();
+            }
         }
     }
 
@@ -50,11 +79,39 @@ public class ParticleBubsTest : MonoBehaviour {
         Vector3 rotateVector = rotation * emitParams.position;
 
         emitParams.position = rotateVector;
+
+        // rotation test for p2 l8r; can't figure out how to programmatically make particles upside down, probably neeed to use velocity instead of just shape...
+       // emitParams.rotation = 180.0f;
+        // emitParams.velocity = emitParams.velocity * -1; //this particle system does not use velocity...
+       
         
 
-        Debug.Log("emitParams.postion: " + emitParams.position.ToString());
+        //Debug.Log("emitParams.postion: " + emitParams.position.ToString());
 
         BubbleParticlePrefab.Emit(emitParams, 1);
     }
 
+
+    public void EmitP2Bubbles()
+    {
+       //Debug.Log("Chris Player2 Bubbles incoming!!!!!!!!!!!!!!!");
+        var emitParams = new ParticleSystem.EmitParams();
+
+        //Debug.Log("mousePostion: " + Input.mousePosition.ToString());
+
+        emitParams.position = Camera.main.ScreenToWorldPoint((Input.mousePosition +
+            new Vector3(0, 0, 5f)));
+
+        // Quaternion to 'undo' -90.0f x rotation units of GameObject transform, or anything with world units.
+        Quaternion rotation = Quaternion.Euler(90f, 0, 0);
+        //  Particle system prefabs like the one used here often are rotated -90 degrees to float 'up';
+        //  this undoes that to work with ScreenToWorldPoint() more naturally without messing with emitParams.rotation
+
+        // Quaternion * vector you're rotating; QuatIdentity must be on LEFT side of multiplication!!!
+        Vector3 rotateVector = rotation * emitParams.position;
+
+        emitParams.position = rotateVector;
+
+        BubblePSystemPlayer2.Emit(emitParams, 1);
+    }
 }
