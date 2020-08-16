2bd1603a140daf1e3fadf21e8f8f397d5b1b071a
JP Fallon <JayGarxP@users.noreply.github.com>
2020-08-15 18:11:44 -0700

    add file for RPS microgame scene

---
+++ Assets/SWORDCLASH/Scripts/MicrogameScripts/MicroRPSController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SwordClash
+{
+
+    public class MicroRPSController : MonoBehaviour
+    {
+        // base microgame LOGIC class? using SwordClash namespace but NOT inheriting from monobehaviour;
+        //      derived microgame logic classes will use callback methods in Controller classes???
+        // timer winner loser
+        // controls???
+        // reentry to squid hockey main gameplay
+
+
+        // Use this for initialization
+        void Start()
+        {
+            // start timer 
+            // set countdown healthbar vars
+            // connect with expected buttons / onclick events
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // timer update
+            // healthbar countdown update
+            // process input
+
+            // once picked
+            // display end screen graphics
+            // re-enter gameplay
+        }
+    }
+}
